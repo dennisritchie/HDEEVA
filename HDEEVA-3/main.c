19d0479a7360ef7287fc4ff460dd5a9f9cb02a9a
Thuan T.Nguyen <posixprogrammer@gmail.com>
2014-06-21 10:32:13 +0700

    My changes

--- HDEEVA-3/main.c
+++ HDEEVA-3/main.c
@@ -3,16 +3,27 @@
 #include <stdlib.h>
 #include <string.h>
 //http://curl.haxx.se/libcurl/c/http-post.html
-#define __DATA__ "{\"contact_email\":\"nguyenthaithuanalg@gmail.com\", \"github_url\":\"https://github.com/karesansui/karesansui\"}"
+#define __DATA__ "{\"contact_email\":\"nguyenthaithuanalg@gmail.com\", \"github_url\":\"https://github.com/dennisritchie/HDEEVA.git\"}"
+static size_t write_data(void *ptr, size_t size, size_t nmemb, void *stream)
+{
+  int written = fwrite(ptr, size, nmemb, (FILE *)stream);
+  fprintf(stderr, "%s", (char* ) ptr);
+  return written;
+}
 int main(int argc, char *argv[]){
 
 	  CURL *curl;
 	  CURLcode res;
+	  static const char *headerfilename = "head.out";
+	FILE *headerfile;
+	static const char *bodyfilename = "body.out";
+	FILE *bodyfile;
 	    struct curl_slist *chunk = NULL;
 
 	    chunk = curl_slist_append(chunk, "POST: /eva/001/endpoint HTTP/1.1");
-	    chunk = curl_slist_append(chunk, "Authorization: Basic ZXZhbm9zcGFtQGhkZS5jby5qcDozNTQ4NzQ1OA==");
+	    chunk = curl_slist_append(chunk, "Authorization: Basic bmd1eWVudGhhaXRodWFuYWxnQGdtYWlsLmNvbTo2MjQ5MjI2Mw==");
 	    chunk = curl_slist_append(chunk, "Host: hdegip.appspot.com");
+	    chunk = curl_slist_append(chunk, "Accept: */*");
 	    chunk = curl_slist_append(chunk, "Content-Type: application/json");
 	    char * content_length = (char*) malloc(128);
 	    memset(content_length, 0, 128);
@@ -32,9 +43,28 @@ int main(int argc, char *argv[]){
 	    /* Now specify the POST data */
 	    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, __DATA__);
 	    /* redo request with our own custom Accept: */
-	    res = curl_easy_setopt(curl, CURLOPT_HTTPHEADER, chunk);
 
+	    res = curl_easy_setopt(curl, CURLOPT_HTTPHEADER, chunk);
+	    /* send all data to this function  */
+	    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
 	    /* Perform the request, res will get the return code */
+	    /* open the files */
+	     headerfile = fopen(headerfilename,"wb");
+	     if (headerfile == NULL) {
+	       curl_easy_cleanup(curl);
+	       return -1;
+	     }
+	     bodyfile = fopen(bodyfilename,"wb");
+	     if (bodyfile == NULL) {
+	       curl_easy_cleanup(curl);
+	       return -1;
+	     }
+
+	     /* we want the headers be written to this file handle */
+	     curl_easy_setopt(curl,   CURLOPT_WRITEHEADER, headerfile);
+
+	     /* we want the body be written to this file handle instead of stdout */
+	     curl_easy_setopt(curl,   CURLOPT_WRITEDATA, bodyfile);
 	    res = curl_easy_perform(curl);
 	    /* Check for errors */
 	    if(res != CURLE_OK)
--- HDEEVA/main.c
+++ HDEEVA/main.c
@@ -1,52 +1,129 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define MAX_BUFFER 1024
+typedef struct {
+	unsigned long square;
+	void * next;
+} LINK_LIST_RESTULT;
+LINK_LIST_RESTULT * result = 0;
+LINK_LIST_RESTULT * last_node = 0;
+#define MIN_TEST_CASE 1
+#define MAX_TEST_CASE 100
+#define MIN_TEST_ELEM 1
+#define MAX_TEST_ELEM 100
+
+
 int number_test_case = 0;
 int numbers = 0;
 char num[256];
-void enter_case(int* count);
-void enter_element(int*);
+int  enter_number(int, int, char*);
+void enter_test_cases(int);
+void enter_elements(int, LINK_LIST_RESTULT **);
+unsigned long get_square (char*, int);
+void hdeeva_out_put();
+char buffer[MAX_BUFFER];
+
 int main (int argc, char* argv[]){
-
-	int opt = 0;
-ENTER_NUMBER_OF_TEST_CASE:
-	memset(num, 0, 256);
-	printf("please enter number: ");
-	fgets(num, 256, stdin);
-	sscanf (num,"%d", &number_test_case);
-	if(number_test_case < 1 || number_test_case > 100){
-		system("clear");
-		goto ENTER_NUMBER_OF_TEST_CASE;
-	}
-
-ENTER_NUMBER_OF_TEST_CASE_OPT:
-//	printf("please enter case: %d", (opt + 1));
-//	memset(num, 0, 256);
-//	fgets(num, 256, stdin);
-//	sscanf (num,"%d", &numbers);
-//	if(numbers < 1 || numbers > 100){
-//		goto ENTER_NUMBER_OF_TEST_CASE_OPT;
-//	}
-	enter_case(&opt);
-	if(opt < number_test_case){
-		goto ENTER_NUMBER_OF_TEST_CASE_OPT;
-	}
+/**
+ * Enter number of test case:
+ * */
+	number_test_case = enter_number(MIN_TEST_CASE, MAX_TEST_CASE, "Please enter number of test case: ");
+	enter_test_cases(number_test_case);
+	hdeeva_out_put();
 	return EXIT_SUCCESS;
 }
-void enter_case(int* opt){
-	int element = 0;
-ENTER_NUMBER_ELEMENT:
-	memset(num, 0, 256);
-	printf("please enter number: ");
-	fgets(num, 256, stdin);
-	sscanf (num,"%d", &element);
-	if(element < 1 || element > 100){
-		goto ENTER_NUMBER_ELEMENT;
+int enter_number(int min, int max, char* message){
+	int num = 0;
+ENTER_NUMBER:
+	memset(buffer, 0, MAX_BUFFER);
+	fprintf(stdout, "%s", message);
+	fgets(buffer, MAX_BUFFER, stdin);
+	sscanf (buffer,"%d", &num);
+	if(num < min || num > max){
+		goto ENTER_NUMBER;
 	}
-	enter_element(&element);
-	(*opt)++;
+	return num;
 }
-void enter_element(int* element){
-int count = 0;
 
+void enter_test_cases(int _case){
+	int cur_case = 0;
+	int elements = 0;
+	char message[128];// = "Please enter number for case:";
+
+ENTER_TEST_CASE:
+	memset(message, 0, 128);
+	sprintf(message, "Please enter number of elements for %ith case:", cur_case + 1);
+	elements = enter_number(MIN_TEST_ELEM, MAX_TEST_ELEM, message);
+
+	enter_elements(elements, &last_node);
+	cur_case++;
+	if(cur_case < _case){
+		goto ENTER_TEST_CASE;
+	}
+}
+void enter_elements(int element, LINK_LIST_RESTULT ** rs){
+	unsigned long square = 0;
+	char message[128];
+ENTER_ELEMENTS:
+	memset(message, 0, 128);
+	memset(buffer, 0, MAX_BUFFER);
+	sprintf(message, "Please enter %i element(s): ", element );
+//	printf("please [element] enter elements: ");
+	fprintf(stdout, "%s", message);
+	fgets(buffer, MAX_BUFFER, stdin);
+	square = get_square(buffer, element);
+	if(square < 1){
+		goto ENTER_ELEMENTS;
+	}
+	if((*rs) == 0){
+		(*rs) = (LINK_LIST_RESTULT *) malloc(sizeof(LINK_LIST_RESTULT));
+		(*rs)->next = 0;
+		(*rs)->square = square;
+		result = (*rs);
+	}
+	else{
+		LINK_LIST_RESTULT * t = (LINK_LIST_RESTULT *) malloc(sizeof(LINK_LIST_RESTULT));
+		t->next = 0;
+		t->square = square;
+		(* rs)->next = t;
+		(* rs) = t;
+	}
+}
+unsigned long get_square (char* data, int element){
+	unsigned long rs = 0;
+	char * pch;
+	int temp = 0;
+	int status = 0;
+	int i = 0;
+	pch = strtok (data," ");
+GET_SQUARE:
+	if(pch != 0){
+		status = sscanf (pch,"%i", &temp);
+		if(status < 1){
+			return 0;
+		}
+		if(temp > 0){
+			rs += (temp * temp);
+		}
+		pch = strtok (0," ");
+		++i;
+		goto GET_SQUARE;
+	}
+	if(i != element){
+		fprintf(stdout, "Wrong entering. Please retry.\n");
+		return 0;
+	}
+	return rs;
+}
+void hdeeva_out_put(){
+	fprintf(stdout, "Sample Output:\n");
+	LINK_LIST_RESTULT * t = result;
+HDEEVA_OUT_PUT:
+	if(t != 0){
+		fprintf(stdout, "%u\n", t->square);
+		t = (LINK_LIST_RESTULT * )t->next;
+		goto HDEEVA_OUT_PUT;
+	}
+//Should free here
 }
