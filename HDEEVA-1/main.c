ed1e77d6134dbf42787fe144f5bd55fbf4e1a4ac
Thuan T.Nguyen <posixprogrammer@gmail.com>
2014-06-22 16:46:41 +0700

    My changes

--- HDEEVA-1/main.c
+++ HDEEVA-1/main.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #define MAX_BUFFER 1024
 typedef struct {
-	unsigned long square;
+	unsigned long long square;
 	void * next;
 } LINK_LIST_RESTULT;
 LINK_LIST_RESTULT * result = 0;
@@ -12,15 +12,12 @@ LINK_LIST_RESTULT * last_node = 0;
 #define MAX_TEST_CASE 100
 #define MIN_TEST_ELEM 1
 #define MAX_TEST_ELEM 100
-
-
 int number_test_case = 0;
 int numbers = 0;
-char num[256];
 int  enter_number(int, int, char*);
 void enter_test_cases(int);
 void enter_elements(int, LINK_LIST_RESTULT **);
-unsigned long get_square (char*, int);
+unsigned long long get_square (char*, int);
 void hdeeva_out_put();
 char buffer[MAX_BUFFER];
 
@@ -28,10 +25,10 @@ int main (int argc, char* argv[]){
 /**
  * Enter number of test case:
  * */
-	fprintf(stdout, "Sample Input:\n");
+	fprintf(stdout, "Please read readme.text.\n Sample Input:\n");
 	number_test_case = enter_number(MIN_TEST_CASE, MAX_TEST_CASE, "Please enter number of test case: ");
 /*
- *
+ * Start to enter test cases:
  * **/
 	enter_test_cases(number_test_case);
 	hdeeva_out_put();
@@ -45,16 +42,15 @@ ENTER_NUMBER:
 	fgets(buffer, MAX_BUFFER, stdin);
 	sscanf (buffer,"%d", &num);
 	if(num < min || num > max){
+		fprintf(stdout, "Wrong entering. Please retry.\n");
 		goto ENTER_NUMBER;
 	}
 	return num;
 }
-
 void enter_test_cases(int _case){
 	int cur_case = 0;
 	int elements = 0;
 	char message[128];
-
 ENTER_TEST_CASE:
 	memset(message, 0, 128);
 //	sprintf(message, "Please enter number of elements for %ith case:", cur_case + 1);
@@ -67,7 +63,7 @@ ENTER_TEST_CASE:
 	}
 }
 void enter_elements(int element, LINK_LIST_RESTULT ** rs){
-	unsigned long square = 0;
+	unsigned long long square = 0;
 	char message[128];
 ENTER_ELEMENTS:
 	memset(message, 0, 128);
@@ -77,6 +73,7 @@ ENTER_ELEMENTS:
 	fgets(buffer, MAX_BUFFER, stdin);
 	square = get_square(buffer, element);
 	if(square < 1){
+		fprintf(stdout, "Wrong entering. Please retry.\n");
 		goto ENTER_ELEMENTS;
 	}
 	if((*rs) == 0){
@@ -93,8 +90,8 @@ ENTER_ELEMENTS:
 		(* rs) = t;
 	}
 }
-unsigned long get_square (char* data, int element){
-	unsigned long rs = 0;
+unsigned long long get_square (char* data, int element){
+	unsigned long long rs = 0;
 	char * pch;
 	int temp = 0;
 	int status = 0;
@@ -107,7 +104,7 @@ GET_SQUARE:
 			return 0;
 		}
 		if(temp > 0){
-			rs += (temp * temp);
+			rs += (unsigned long long)((unsigned long long)temp * (unsigned long long)temp);
 		}
 		pch = strtok (0," ");
 		++i;
@@ -124,7 +121,7 @@ void hdeeva_out_put(){
 	LINK_LIST_RESTULT * t = result;
 HDEEVA_OUT_PUT:
 	if(t != 0){
-		fprintf(stdout, "%u\n", t->square);
+		fprintf(stdout, "%lld\n", t->square);
 		t = (LINK_LIST_RESTULT * )t->next;
 		goto HDEEVA_OUT_PUT;
 	}
